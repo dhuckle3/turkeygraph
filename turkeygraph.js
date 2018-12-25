c4e6b527c6d513ae5c3d1f07df809b324a553839
Donald Huckle <dhuckle3@gmail.com>
2018-12-25 14:34:15 -0500

    Lift some stuff from the d3 regression example

--- turkeygraph.js
+++ turkeygraph.js
@@ -1,6 +1,14 @@
 var bin = '5bf6dd0e8ae0925944e03a3d';
 var secret_key = '$2a$10$mNtLmo.yrgNyKJL19TxJ/.HDWbJjJ0b1ue8IrV9SxXW6hchvL48Iy'; // not so secret yet
 var data = [];
+var margin = {
+    top: 20,
+    right: 20,
+    bottom: 30,
+    left: 40
+},
+width = 600 - margin.left - margin.right,
+height = 400 - margin.top - margin.bottom;
 
 function get_bin() {
     $.ajax({
@@ -39,7 +47,6 @@ function add_point(event) {
     [hour, minute] = document.getElementById('time').value.split(':');
     var time = parseInt(hour)*60 + parseInt(minute)
     this.data.push({time: time , temp: temp});
-
     update_bin(data);
     update();
 }
@@ -97,84 +104,91 @@ function update() {
     if (this.data.length == 0) {
         return;
     }
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, c.width, c.height);
 
-    var margin = 50;
-    var width = c.width - (margin * 2)
-    var height = c.height - (margin * 2);
+    var x = d3.scale.linear()
+    .range([0, width]);
 
-    // Draw axis lines 
-    ctx.beginPath();
-    ctx.moveTo(margin-10, margin+10)
-    ctx.lineTo(margin, margin);
-    ctx.moveTo(margin+10, margin+10)
-    ctx.lineTo(margin, margin);
-    ctx.lineTo(margin, height+margin);
-    ctx.lineTo(width+margin, height+margin);
-    ctx.lineTo(width+margin-10, height+margin-10)
-    ctx.moveTo(width+margin-10, height+margin+10)
-    ctx.lineTo(width+margin, height+margin);
-    ctx.stroke();
+    var y = d3.scale.linear()
+    .range([height, 0]);
 
-    // TODO draw vertical tick marks
-    // TODO draw horizontal tick marks
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
 
-    // Calculate time scale
-    var timescale = time_scale(data, width);
-    var tempscale = temp_scale(data, height);
-    var mintime = min_time(data);
-    var mintemp = min_temp(data);
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
 
-    for (var i = 0; i < data.length; i++) {
-        var x = margin + Math.floor((data[i].time-mintime) * timescale);
-        var y = margin + height - (Math.floor((data[i].temp-mintemp) * tempscale));
-        ctx.fillRect(x-5, y-5, 10, 10);
-    }
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    [a, b] = calculate_regression();
+    var data = format_data(this.data);
 
-    // draw the regression line
-    // y = a + bx
-    // x = (y - a)/b
-    ctx.beginPath();
-    ctx.moveTo(margin, margin + height - (Math.floor(a - mintemp)*tempscale));
+    data.forEach(function(d) {
+        d.x = +d.x;
+        d.y = +d.y;
+        d.yhat = +d.yhat;
+    });
 
-    // The turkey is 165 in breast
-    var x = Math.floor((165 - a) / b);
-    ctx.lineTo(margin + Math.floor((x - mintime) * timescale), margin + height - Math.floor((165 - mintemp) * tempscale));
-    ctx.stroke();
-    // guess when the turkey will be done...
+    var line = d3.svg.line()
+        .x(function(d) {
+            return x(d.x);
+        })
+        .y(function(d) {
+            return y(d.yhat);
+        });
 
-    var result = "The turkey will be done at: " + Math.floor(x/60) + ":" + x%60;
-    $( "#result" ).html(result);
+    x.domain(d3.extent(data, function(d) {
+        return d.x;
+    }));
+
+    y.domain(d3.extent(data, function(d) {
+        return d.y;
+    }));
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("Time (hour*60 + minutes)");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Temperature (F)")
+
+    svg.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 3.5)
+        .attr("cx", function(d) {
+            return x(d.x);
+        })
+        .attr("cy", function(d) {
+            return y(d.y);
+        });
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
 }
 this.update = update.bind(this);
-
-function calculate_regression() {
-    var sigmaX = 0;
-    var sigmaY = 0;
-    var sigmaXY = 0;
-    var sigmaY2 = 0;
-    var sigmaX2 = 0;
-    for (var i = 0; i < data.length; i++) {
-        var x = data[i].time;
-        var y = data[i].temp;
-
-        sigmaY += y;
-        sigmaX += x;
-        sigmaXY += x * y;
-        sigmaX2 += x * x;
-        sigmaY2 += y * y;
-    }
-    var n = data.length;
-    var a = ((sigmaY * sigmaX2) - (sigmaX * sigmaXY)) / ((n * sigmaX2) - (sigmaX * sigmaX));
-    var b = ((n * sigmaXY) - (sigmaX * sigmaY)) / ((n * sigmaX2) - (sigmaX * sigmaX));
-    return [a, b];
-}
-
-
 
 document.getElementById("add-point").addEventListener("click", add_point.bind(this));
 document.getElementById("clear-data").addEventListener("click", clear.bind(this));
@@ -193,3 +207,63 @@ $(function(){
 });
 
 get_bin();
+
+function format_data(binData) {
+    var x = [];
+    var y = [];
+    var x_mean = 0;
+    var y_mean = 0;
+    n = binData.length;
+    var term1 = 0;
+    var term2 = 0;
+
+    for (var i = 0; i < n; i++) {
+        point = binData[i]
+        y.push(point.temp)
+        x.push(point.time)
+        x_mean += x[i]
+        y_mean += y[i]
+    }
+    // calculate mean x and y
+    x_mean /= n;
+    y_mean /= n;
+
+    // calculate coefficients
+    var xr = 0;
+    var yr = 0;
+    for (i = 0; i < x.length; i++) {
+        xr = x[i] - x_mean;
+        yr = y[i] - y_mean;
+        term1 += xr * yr;
+        term2 += xr * xr;
+
+    }
+    var b1 = term1 / term2;
+    var b0 = y_mean - (b1 * x_mean);
+
+    yhat = [];
+    // fit line using coeffs
+    for (i = 0; i < x.length; i++) {
+        yhat.push(b0 + (x[i] * b1));
+    }
+
+    var data = [];
+    for (i = 0; i < y.length; i++) {
+        data.push({
+            "yhat": yhat[i],
+            "y": y[i],
+            "x": x[i]
+        })
+    }
+
+    // Guess when the turkey will be done
+    // y = mx + b, x = (y-b)/m
+    time_done =  Math.floor((165 - b0) / b1);
+    console.log(time_done);
+    var result = "The turkey will be done at: " + Math.floor(time_done/60) + ":" + time_done%60;
+    $( "#result" ).html(result);
+    return (data);
+}
+
+// much lifted from https://bl.ocks.org/ctufts/298bfe4b11989960eeeecc9394e9f118
+// which was licensed under GNU General Public License, version 3.
